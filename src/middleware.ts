3fc8c3d4c9a083c3d22ab758d09d5ced7291d764
devkgm <gm.kim333@gmail.com>
2024-11-28 22:31:17 +0900

    fix 업로드, 로깅 매칭 제외

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,6 +35,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|upload|log|favicon.ico).*)',
   ],
 }; 
