f97c4741c9da69736db123633735ecc6f14c78c9
devkgm <gm.kim333@gmail.com>
2025-08-23 21:37:04 +0900

    feat dockerfile 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ["avatars.githubusercontent.com"],
   },
+  output: "standalone",
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
